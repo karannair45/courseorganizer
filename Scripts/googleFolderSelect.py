4e36bc70dc2447fd132b7a842ad9c93d564ac07c
karannair45 <123226537+karannair45@users.noreply.github.com>
2023-02-01 05:45:38 -0500

    Update googleFolderSelect.py

--- Scripts/googleFolderSelect.py
+++ Scripts/googleFolderSelect.py
@@ -3,8 +3,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from docx import Document
-import time
+import numpy as np
 
 CLIENT_SECRET_FILE = 'client_secrets.json'
 API_NAME = 'drive'
@@ -49,9 +48,6 @@ def create_folder(fname):
     }
     service.files().create(body=file_metadata).execute()
 
-#columns and index
-letters = ["A","B","C","E","F","G","H","I"]
-
 r = 0               # Keeps track of rows
 weekCount = 1       # Keeps track of weeks
 lab = 1             # Keeps track of labs
@@ -59,10 +55,24 @@ hw = 1              # Keeps track of homeworks
 lec = 1             # Keeps track of lectures
 w = 2               # Keep track of biweekly count
 rowRange = len(worksheet.get_all_values()) # Number of rows
-
+iter = 1
 remainder = (rowRange - 1) % 2
 
 maxRow = (((rowRange - 1) // 2) + remainder)
+
+array = np.array(worksheet.get_all_values())
+
+arr = []
+
+for x in array:
+    for y in x:
+        arr.append(y)
+
+def remove():
+    for element in range(9):
+        arr.pop(0)
+
+remove()
 
 # Parse through worksheet to manipulate cells
 for row in range(maxRow):
@@ -74,59 +84,63 @@ for row in range(maxRow):
         weekCount += 1
         create_folder(weekPath)
         for week in range(2):
-            i = 1
-            while i < len(letters):
-                c1 = worksheet.acell(letters[i] + str(w)).value
-                c2 = worksheet.acell('F' + str(w)).value
-                c3 = worksheet.acell('H' + str(w)).value
-                c4 = worksheet.acell('I' + str(w)).value
-                c5 = worksheet.acell('C' + str(w)).value
-                c6 = worksheet.acell('G' + str(w)).value
-                c7 = worksheet.acell('E' + str(w)).value
-                if i == 1:
-                    if c1 is not None:
-                        file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'reading' + str(lec) +".txt", 'mimeType':'text/csv'})
-                        if c7 is not None:
-                            file1.SetContentString(c7 + " ")
-                        if c2 is not None:
-                            file1.SetContentString(c2 + " ")
-                        if c3 is not None:
-                            file1.SetContentString(' HW' + c3 + ' is due ' + worksheet.acell['A' + str(w)].value)
-                        if c4 is not None:
-                            file1.SetContentString(' Lab' + c4 + ' is due ' + worksheet.acell['A' + str(w)].value)
+            if len(arr) > 0:
+                if len(arr[1]) > 0:
+                    file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'reading' + str(lec) +".txt", 'mimeType':'text/csv'})
+                    if len(arr[4]) > 0:
+                        file1.SetContentString(arr[4] + " ")
+                        if len(arr[5]) > 0:
+                            file1.SetContentString(arr[4] + " " + arr[5])
+                        if len(arr[7]) > 0 :
+                            file1.SetContentString(arr[4] + " " + arr[5] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                            if len(arr[5]) > 0:
+                                file1.SetContentString(arr[4] + " " + arr[5] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                        if len(arr[8]) > 0:
+                            file1.SetContentString(arr[4] + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                            if len(arr[5]) > 0:
+                                file1.SetContentString(arr[4] + " " + arr[5] + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                            if len(arr[7]) > 0:
+                                file1.SetContentString(arr[4] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                                if len(arr[5]) > 0:
+                                    file1.SetContentString(arr[4] + arr[5] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                    file1.Upload()
+                    lec += 1
+                    if len(arr[2]) > 0:
+                        file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'Lab' + str(lab) +".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
+                        lab +=1
                         file1.Upload()
-                        lec += 1
-                        if c5 is not None:
-                            file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'Lab' + str(lab) +".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
-                            lab +=1
-                            file1.Upload()
-                        if c6 is not None:
-                            file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'HW' + str(hw) +".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
-                            hw += 1
-                            file1.Upload()                       
-                    elif c1 is None:
-                        file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'reading' + str(lec) +".txt", 'mimeType':'text/csv'})
-                        if c7 is not None:
-                            file1.SetContentString(c7 + " ")
-                        if c2 is not None:
-                            file1.SetContentString(c2 + " ")
-                        if c3 is not None:
-                            file1.SetContentString(' HW' + c3 + ' is due ' + worksheet.acell['A' + str(w)].value)
-                        if c4 is not None:
-                            file1.SetContentString(' Lab' + c4 + ' is due ' + worksheet.acell['A' + str(w)].value)
-                        lec += 1
-                        file1.Upload()   
-                        if c5 is not None:
-                            file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'Lab' + str(lab) + ".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
-                            lab += 1
-                            file1.Upload()
-                        if c6 is not None:
-                            file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'HW' + str(hw) + ".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
-                            hw += 1
-                            file1.Upload()
-                    i+=1
+                    if len(arr[6]) > 0:
+                        file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'HW' + str(hw) +".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
+                        hw += 1
+                        file1.Upload()
+                elif len(arr[1]) > 0:
+                    file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'reading' + str(lec) +".txt", 'mimeType':'text/csv'})
+                    if len(arr[4]) > 0:
+                        file1.SetContentString(arr[4] + " ")
+                        if len(arr[5]) > 0:
+                            file1.SetContentString(arr[4] + " " + arr[5])
+                        if len(arr[7]) > 0 :
+                            file1.SetContentString(arr[4] + " " + arr[5] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                            if len(arr[5]) > 0:
+                                file1.SetContentString(arr[4] + " " + arr[5] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                        if len(arr[8]) > 0:
+                            file1.SetContentString(arr[4] + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                            if len(arr[5]) > 0:
+                                file1.SetContentString(arr[4] + " " + arr[5] + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                            if len(arr[7]) > 0:
+                                file1.SetContentString(arr[4] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                                if len(arr[5]) > 0:
+                                    file1.SetContentString(arr[4] + arr[5] + ' HW' + arr[7] + ' is due ' + worksheet.acell('A' + str(w)).value + ' Lab' + arr[8] + ' is due ' + worksheet.acell('A' + str(w)).value)
+                    file1.Upload()
+                    lec += 1
+                    if len(arr[2]) > 0:
+                        file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'Lab' + str(lab) +".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
+                        lab +=1
+                        file1.Upload()
+                    if len(arr[6]) > 0:
+                        file1 = drive.CreateFile({'parents': [{'id': get_id(weekPath)}],'title': 'HW' + str(hw) +".docx", 'mimeType':'application/vnd.openxmlformats-officedocument.wordprocessingml.document'})
+                        hw += 1
+                        file1.Upload()
                 w += 1
+                remove()
         r +=2
-                        
-                    
-                
