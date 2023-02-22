a70f11ba3d0dfe53ce02f2cbaf1fbac4f164deeb
karannair45 <123226537+karannair45@users.noreply.github.com>
2023-02-22 08:20:18 -0500

    Add files via upload

---
+++ Scripts/formsgoogle.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
+
+SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
+DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+
+store = file.Storage('client_key.json')
+creds = None
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = discovery.build('forms', 'v1', http=creds.authorize(
+    Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
+
+# Prints the responses of your specified form:
+form_id = '17r--cKEI7Xu7ER7DSJg0AWYfrZSuz64bkWJTWX6zE9o'
+result = service.forms().responses().list(formId=form_id).execute()
+print(result)
+
+
