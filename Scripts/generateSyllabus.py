f8df32ddaa4f84b883b0283c3cbe5b2d942d7c23
mahyarkhafaji <123523870+mahyarkhafaji@users.noreply.github.com>
2023-03-07 23:01:18 -0500

    Update generateSyllabus.py

--- Scripts/generateSyllabus.py
+++ Scripts/generateSyllabus.py
@@ -3,6 +3,7 @@ from docx.shared import Inches
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.enum.text import WD_UNDERLINE
 import openpyxl, os
+import pandas as pd
 
 document = Document()
 section = document.sections[0]
@@ -12,14 +13,13 @@ header = section.header
 xl = openpyxl.load_workbook('C:/Users/mahya/Desktop/Capstone/courseorganizer-main/Scripts/Website_Questionnaire_Form.xlsx')
 sheet = xl['Sheet1']
 
+# Load schedule excel
+df = pd.read_excel('C:/Users/mahya/Desktop/Capstone/courseorganizer-main/Scripts/test.xlsx', sheet_name='Sheet1')
+
 #columns and index
 letters = ["B", "C", "D", "E", "F", "G", "H","I", "J", "AL", "AV", "AW", "AX", "AY", "AU", "AT", "AN", "AO", "AP", 
            "AQ", "AS", "AR", "AM", "AH", "AJ", "AK", "AI", "AB", "AC", "Y", "AD", "AE", "AF", "AA", "AG", "X", "W", 
-           "N", "Z", "O", "P", "R", "Q", "V", "S", "T", "U", "K", "L", "M"]
-
-remainder = (sheet.max_row - 1) % 2
-
-maxRow = (((sheet.max_row - 1) // 2) + remainder)
+           "N", "Z", "O", "P", "R", "Q", "V", "S", "T", "U", "K", "L", "M", "AZ", "BA", "BB", "BC"]
 
 answers = []
 
@@ -48,7 +48,7 @@ title3_format = title3.paragraph_format
 title3_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 records = [
-    ('Class Location: ' + answers[4], 'E-mail: '),
+    ('Class Location: ' + answers[4], 'E-mail: ' + answers[50]),
     ('Office location: ' + answers[7], 'Office hours: ' + answers[6]),
     
 ]
@@ -56,9 +56,9 @@ records = [
 if (answers[14].upper() == 'Yes'):
     records.append('Phone: ' + answers[14])
 if (answers[20].upper() == 'Yes'):
-    records.append('Lab location: ')
+    records.append('Lab location: ' + answers[51])
 if (answers[22].upper() == 'Yes'):
-    records.append(('Discussion seminar time: ', 'Discussion Seminar location: '))
+    records.append(('Discussion seminar time: ' + answers[52], 'Discussion Seminar location: ' + answers[53]))
 if (answers[16].upper() == 'Yes'):
     records.append('Virtual office hours(Zoom)\nMeeting ID: '+ answers[17] + '\nPasscode: \n' + answers[18])
 
@@ -73,7 +73,7 @@ for x, y in records:
 
 paragraph2 = document.add_paragraph('\n')
 
-if (answers[28].upper() == 'Yes'):
+if (answers[28].upper() == 'YES'):
     run = paragraph2.add_run('Prerequisites')
     run.underline = True
     run.underline = WD_UNDERLINE.SINGLE
@@ -83,24 +83,24 @@ paragraph2.add_run('\n')
 paragraph2.add_run('\nCourse Description').underline = WD_UNDERLINE.SINGLE
 paragraph2.add_run('\n' + answers[12])
 
-if (answers[30].upper() == 'Yes'):
+if (answers[30].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nCourse Materials').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[31])
-    paragraph2.add_run('\n' + answers[32])
+    paragraph2.add_run('\n' 'Textbook: ' + answers[31])
+    paragraph2.add_run('\n' 'ISBN: ' + answers[32])
 
-if (answers[33].upper() == 'Yes'):
+if (answers[33].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nLearning Objectives').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\nStudents who successfully complete this course will achieve the following learning objectives:')
     paragraph2.add_run('\n' + answers[34])
 
-if (answers[20].upper() == 'Yes'):
+if (answers[20].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nLab Policy').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[21])
 
-if (answers[35].upper() == 'Yes'):
+if (answers[35].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nAssignments').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[36])
@@ -109,17 +109,17 @@ paragraph2.add_run('\n')
 paragraph2.add_run('\nExpectations').underline = WD_UNDERLINE.SINGLE
 paragraph2.add_run('\n' + answers[37])
 
-if (answers[38].upper() == 'Yes'):
+if (answers[38].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nQuiz').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[39])
 
-if (answers[40].upper() == 'Yes'):
+if (answers[40].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nExam').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[41])
 
-if (answers[22].upper() == 'Yes'):
+if (answers[22].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nDiscussion Policy').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[23])
@@ -141,12 +141,12 @@ paragraph2.add_run('\n')
 paragraph2.add_run('\nHonor Code').underline = WD_UNDERLINE.SINGLE
 paragraph2.add_run('\n' + answers[9])
 
-if (answers[44].upper() == 'Yes'):
+if (answers[44].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nOnline Resources').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[45])
 
-if (answers[46].upper() == 'Yes'):
+if (answers[46].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nExtra Credit').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\n' + answers[47])
@@ -163,42 +163,27 @@ paragraph2.add_run('\n')
 paragraph2.add_run('\nWithdrawals').underline = WD_UNDERLINE.SINGLE
 paragraph2.add_run('\n' + answers[49])
 
-if (answers[27].upper() == 'Yes'):
+if (answers[27].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nSyllabus Update').underline = WD_UNDERLINE.SINGLE
     paragraph2.add_run('\nThis syllabus can be updated at any point in time in the semester')
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nLecture Schedule').underline = WD_UNDERLINE.SINGLE
-schedule = (
-    ('12/7', 'Mon', 'Lab1', 'Virus', 'Chapter 1', 'HW 1', '', '', '', ''),
-    ('12/9', 'Wed', 'Lab1', 'Virus', '' ,'' ,'' ,'HW1' , '', 'Quiz 1')
-)
 
-table2 = document.add_table(rows=1, cols=10, style = "Table Grid")
-row = table2.rows[0].cells
-row[0].text = 'Date'
-row[1].text = 'Lecture'
-row[2].text = 'Lab'
-row[3].text = 'Topic'
-row[4].text = 'Reading'
-row[5].text = 'HW'
-row[6].text = 'HW Due'
-row[7].text = 'Lab Due'
-row[8].text = 'Exam'
-row[9].text = 'Quiz'
-for one, two, three, four, five, six, seven, eight, nine, ten in schedule:
+#schedule table
+df.fillna('', inplace=True)
+table2 = document.add_table(rows=1, cols=len(df.columns), style = "Table Grid")
+
+# Add header row to the table
+hdr_cells = table2.rows[0].cells
+for i in range(len(df.columns)):
+    hdr_cells[i].text = df.columns[i]
+
+# Add data to the table
+for index, row in df.iterrows():
     row_cells = table2.add_row().cells
-    row_cells[0].text = one
-    row_cells[1].text = two
-    row_cells[2].text = three
-    row_cells[3].text = four
-    row_cells[4].text = five
-    row_cells[5].text = six
-    row_cells[6].text = seven
-    row_cells[7].text = eight
-    row_cells[8].text = nine
-    row_cells[9].text = ten
-
+    for i in range(len(df.columns)):
+        row_cells[i].text = str(row[i])
 
 document.save('syllabus.docx')
