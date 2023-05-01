d4f63b14dc9b3fe2e15b125ca3ced1d7c2f1d10d
mahyarkhafaji <123523870+mahyarkhafaji@users.noreply.github.com>
2023-05-01 15:46:58 -0400

    Update generateSyllabus.py

--- Scripts/generateSyllabus.py
+++ Scripts/generateSyllabus.py
@@ -11,47 +11,45 @@ header = section.header
 
 f = open( "q&a.txt", "r")
 g = f.read()
-answers = [s.replace('"', '').replace("[", "").replace("]", "") for s in g.split(',')]
+answers = [s.replace("[", "").replace("]", "") for s in g.split('"')]
 
 # Load schedule excel
 df = pd.read_excel('C:/Users/mahya/Desktop/Capstone/courseorganizer-main/Scripts/test.xlsx', sheet_name='Sheet1')
 
 # Template of the syllabus
 paragraph = header.paragraphs[0]
-paragraph.text = "Course Syllabus\t\t" + answers[7]
+paragraph.text = "Course Syllabus\t\t" + answers[23]
 paragraph.style = document.styles["Header"]
 
-title = document.add_paragraph(answers[0])
+title = document.add_paragraph(answers[1])
 title_format = title.paragraph_format
 title_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-title2 = document.add_paragraph(answers[1])
+title2 = document.add_paragraph(answers[2])
 title2_format = title2.paragraph_format
 title2_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-title3 = document.add_paragraph(answers[3])
+title3 = document.add_paragraph(answers[13])
 title3_format = title3.paragraph_format
 title3_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 records = [
-    ('Class Location: ' + answers[4], 'E-mail: ' + answers[50]),
-    ('Office location: ' + answers[7], 'Office hours: ' + answers[6]),
+    ('Class Location: ' + answers[15], 'E-mail: ' + answers[11]),
+    ('Office location: ' + answers[21], 'Office hours: ' + answers[19]),
     
 ]
 
-if (answers[14].upper() == 'Yes'):
-    records.append('Phone: ' + answers[14])
-if (answers[20].upper() == 'Yes'):
-    records.append('Lab location: ' + answers[51])
-if (answers[22].upper() == 'Yes'):
-    records.append(('Discussion seminar time: ' + answers[52], 'Discussion Seminar location: ' + answers[53]))
-if (answers[16].upper() == 'Yes'):
-    records.append('Virtual office hours(Zoom)\nMeeting ID: '+ answers[17] + '\nPasscode: \n' + answers[18])
+if (answers[14].upper() == 'YES'):
+    records.append(('Phone: ' + answers[9], ''))
+if (answers[22].upper() == 'YES'):
+    records.append(('Discussion seminar time: ' + answers[49], 'Discussion Seminar location: ' + answers[51]))
+if (answers[35].upper() == 'YES'):
+    records.append('Virtual office hours(Zoom)\nMeeting ID: '+ answers[37] + '\nPasscode: \n' + answers[39], '')
 
 table = document.add_table(rows=1, cols=2, style = "Table Grid")
 row = table.rows[0].cells
-row[0].text = 'Instructor: ' + answers[2]
-row[1].text = 'Class Time: ' + answers[5]
+row[0].text = 'Instructor: ' + answers[5]
+row[1].text = 'Class Time: ' + answers[17]
 for x, y in records:
     row_cells = table.add_row().cells
     row_cells[0].text = x
@@ -59,95 +57,94 @@ for x, y in records:
 
 paragraph2 = document.add_paragraph('\n')
 
-if (answers[28].upper() == 'YES'):
+if (answers[77].upper() == 'YES'):
     run = paragraph2.add_run('Prerequisites')
     run.underline = True
     run.underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[29])
+    paragraph2.add_run('\n' + answers[79])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nCourse Description').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[12])
+paragraph2.add_run('\n' + answers[31])
 
-if (answers[30].upper() == 'YES'):
+if (answers[81].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nCourse Materials').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' 'Textbook: ' + answers[31])
-    paragraph2.add_run('\n' 'ISBN: ' + answers[32])
+    paragraph2.add_run('\n' 'Textbook: ' + answers[83])
+    paragraph2.add_run('\n' 'ISBN: ' + answers[85])
 
-if (answers[33].upper() == 'YES'):
+if (answers[87].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nLearning Objectives').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\nStudents who successfully complete this course will achieve the following learning objectives:')
-    paragraph2.add_run('\n' + answers[34])
+    paragraph2.add_run('\n' + answers[89])
 
-if (answers[20].upper() == 'YES'):
+if (answers[43].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nLab Policy').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[21])
+    paragraph2.add_run('\n' + answers[45])
 
-if (answers[35].upper() == 'YES'):
+if (answers[59].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nAssignments').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[36])
+    paragraph2.add_run('\n' + answers[61])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nExpectations').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[37])
+paragraph2.add_run('\n' + answers[91])
 
-if (answers[38].upper() == 'YES'):
+if (answers[63].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nQuiz').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[39])
+    paragraph2.add_run('\n' + answers[65])
 
-if (answers[40].upper() == 'YES'):
+if (answers[69].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nExam').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[41])
+    paragraph2.add_run('\n' + answers[71])
 
-if (answers[22].upper() == 'YES'):
+if (answers[47].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nDiscussion Policy').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[23])
+    paragraph2.add_run('\n' + answers[49])
 
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nAttendance').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[11])
+paragraph2.add_run('\n' + answers[29])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nGrading').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[43])
+paragraph2.add_run('\n' + answers[95])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nDisability Services').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[10])
+paragraph2.add_run('\n' + answers[27])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nHonor Code').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[9])
+paragraph2.add_run('\n' + answers[25])
 
-if (answers[44].upper() == 'YES'):
+if (answers[97].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nOnline Resources').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[45])
+    paragraph2.add_run('\n' + answers[99])
 
-if (answers[46].upper() == 'YES'):
+if (answers[101].upper() == 'YES'):
     paragraph2.add_run('\n')
     paragraph2.add_run('\nExtra Credit').underline = WD_UNDERLINE.SINGLE
-    paragraph2.add_run('\n' + answers[47])
+    paragraph2.add_run('\n' + answers[103])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nFinal Exam').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[48])
+paragraph2.add_run('\n' + answers[105])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nInclement Weather').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[13])
+paragraph2.add_run('\n' + answers[33])
 
 paragraph2.add_run('\n')
 paragraph2.add_run('\nWithdrawals').underline = WD_UNDERLINE.SINGLE
-paragraph2.add_run('\n' + answers[49])
+paragraph2.add_run('\n' + answers[107])
 
 if (answers[27].upper() == 'YES'):
     paragraph2.add_run('\n')
